7e74df4dcd21b778b17beeb87c5e14a1e5e4e6af
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-07-21 14:11:44 +0800

    [skip ci] Automated clean.

--- R/sigma2.R
+++ R/sigma2.R
@@ -10,13 +10,17 @@
 #' @keywords model-implied, parameters
 #' @inheritParams slopes
 #' @inheritParams Sigmatheta
+#' @param sigma2y Numeric.
+#'   Variance of the regressand variable \eqn{\left( \sigma_{y}^{2} \right)}.
+#' @export
 sigma2 <- function(slopes,
+                   sigma2y,
                    sigmayX,
                    SigmaX) {
-  foo <- function(Sigmatheta,
-                  A) {
-    invIminusA <- solve(I - A)
-    invIminusA %*% Sigmatheta %*% t(invIminusA)
+  foo <- function(A,
+                  S) {
+    invIminusA <- solve(diag(nrow(A)) - A)
+    invIminusA %*% S %*% t(invIminusA)
   }
   slopes <- as.vector(slopes)
   p <- length(slopes)
@@ -30,13 +34,32 @@ sigma2 <- function(slopes,
     0,
     slopes
   )
+  filter <- diag(nrow(A))
   Sigmatheta <- cbind(0, SigmaX)
   Sigmatheta <- rbind(0, Sigmatheta)
-  Sigmatheta[1, ] <- sigmayX
-  Sigmatheta[, 1] <- sigmayX
-  S <- foo(
-    Sigmatheta = Sigmatheta,
-    A = A
+  Sigmatheta[1:k, 1] <- c(sigma2y, sigmayX)
+  Sigmatheta[1, 1:k] <- c(sigma2y, sigmayX)
+  # get variances
+  sigma2 <- diag(Sigmatheta)
+  # construct S matrix
+  ## from Sigmatheta
+  S <- Sigmatheta
+  ## zeroes on y row and column
+  S[1, ] <- 0
+  S[, 1] <- 0
+  ## zeroes on diagonal
+  diag(S) <- 0
+  ## initial Sigmatheta
+  Sigmatheta_temp <- foo(
+    A = A,
+    S = S
   )
+  for (i in k:1) {
+    S[i, i] <- sigma2[i] - Sigmatheta_temp[i, i]
+    Sigmatheta_temp <- foo(
+      A = A,
+      S = S
+    )
+  }
   S[1, 1]
 }
