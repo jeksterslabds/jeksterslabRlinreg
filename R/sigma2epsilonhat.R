d1c145d8d4c8c3801df9e1dcc65021d3ce3fe1d4
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-07-26 21:59:55 +0800

    Separated biased and unbiased.

--- R/sigma2epsilonhat.R
+++ R/sigma2epsilonhat.R
@@ -2,37 +2,29 @@
 #'
 #' @title Residual Variance \eqn{\hat{\sigma}_{\varepsilon}^{2}} (from \eqn{RSS})
 #'
-#' @description Calculates estimates of the error variance
+#' @description Calculates an estimate of the error variance
 #'   \deqn{
 #'     \mathbf{E} \left( \sigma^2 \right) = \hat{\sigma}_{\varepsilon}^{2}
 #'   }
 #'   \deqn{
-#'     \hat{\sigma}_{\varepsilon \textrm{unbiased}}^{2}
+#'     \hat{\sigma}_{\varepsilon}^{2}
 #'     = \frac{1}{n - k} \sum_{i = 1}^{n} \left( \mathbf{y} - \mathbf{X} \boldsymbol{\hat{\beta}} \right)^2 \\
 #'     = \frac{\boldsymbol{\varepsilon}^{\prime} \boldsymbol{\varepsilon}}{n - k} \\
 #'     = \frac{RSS}{n - k}}
-#'   or
-#'   \deqn{
-#'     \hat{\sigma}_{\varepsilon \textrm{biased}}^{2}
-#'     = \frac{1}{n} \sum_{i = 1}^{n} \left( \mathbf{y} - \mathbf{X} \boldsymbol{\hat{\beta}} \right)^2 \\
-#'     = \frac{\boldsymbol{\varepsilon}^{\prime} \boldsymbol{\varepsilon}}{n} \\
-#'     = \frac{RSS}{n}
-#'   }
 #'   where
 #'   \eqn{\boldsymbol{\varepsilon}} is the vector of residuals,
 #'   \eqn{RSS} is the residual sum of squares,
 #'   \eqn{n} is the sample size,
 #'   and \eqn{k} is the number of regressors
-#'   including a regressor whose value is 1 for each observation.
+#'   including a regressor whose value is 1 for each observation on the first column.
 #'
-#' @details If `RSS = NULL`, `RSS` is computed using [`RSS()`]
-#'   with `X` and `y` as a required arguments and `betahat` as an optional argument.
-#'   If `RSS` is provided, `betahat`, `X`, and `y` are not needed.
+#' @details If `RSS = NULL`, `RSS` is computed using [`RSS()`].
+#'   If `RSS` is provided, `X`, and `y` are not needed.
 #'
 #' @family residual variance functions
 #' @keywords sigma2epsilonhat
 #' @inheritParams yminusyhat
-#' @inheritParams .RSS
+#' @inheritParams RSS
 #' @inherit yhat references
 #' @param RSS Numeric.
 #'   Residual sum of squares.
@@ -40,50 +32,65 @@
 #'   Sample size.
 #' @param k Integer.
 #'   Number of regressors including a regressor
-#'   whose value is 1 for each observation.
-#' @param type String.
-#'   Residual variance estimator.
-#'   If `type = "unbiased"`, returns unbiased estimate.
-#'   If `type = "biased"`, returns biased estimate.
-#'   If `type = "both"`, returns a vector of unbiased and biased estimates.
+#'   whose value is 1 for each observation on the first column.
 #' @return Returns the estimated residual variance \eqn{\hat{\sigma}_{\varepsilon}^{2}} .
 #' @export
 .sigma2epsilonhat <- function(RSS = NULL,
                               n,
                               k,
-                              type = "unbiased",
-                              epsilonhat = NULL,
-                              X = NULL,
-                              y = NULL,
-                              betahat = NULL) {
-  if (!is.null(X) & !is.null(y)) {
+                              X,
+                              y) {
+  if (is.null(RSS)) {
+    RSS <- RSS(
+      X = X,
+      y = y
+    )
     n <- nrow(X)
     k <- ncol(X)
   }
+  RSS / (n - k)
+}
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Residual Variance \eqn{\hat{\sigma}_{\varepsilon \ \textrm{biased}}^{2}} (from \eqn{RSS})
+#'
+#' @description Calculates an estimate of the error variance
+#'   \deqn{
+#'     \mathbf{E} \left( \sigma^2 \right) = \hat{\sigma}_{\varepsilon}^{2}
+#'   }
+#'   \deqn{
+#'     \hat{\sigma}_{\varepsilon \ \textrm{biased}}^{2}
+#'     = \frac{1}{n} \sum_{i = 1}^{n} \left( \mathbf{y} - \mathbf{X} \boldsymbol{\hat{\beta}} \right)^2 \\
+#'     = \frac{\boldsymbol{\varepsilon}^{\prime} \boldsymbol{\varepsilon}}{n} \\
+#'     = \frac{RSS}{n}
+#'   }
+#'   where
+#'   \eqn{\boldsymbol{\varepsilon}} is the vector of residuals,
+#'   \eqn{RSS} is the residual sum of squares, and
+#'   \eqn{n} is the sample size.
+#'
+#' @family residual variance functions
+#' @keywords sigma2epsilonhat
+#' @inheritParams yminusyhat
+#' @inheritParams RSS
+#' @inheritParams .sigma2epsilonhat
+#' @inherit yhat references
+#' @inherit .sigma2epsilonhat details
+#' @return Returns the estimated residual variance \eqn{\hat{\sigma}_{\varepsilon \ \textrm{biased}}^{2}} .
+#' @export
+.sigma2epsilonhatbiased <- function(RSS = NULL,
+                                    n,
+                                    X,
+                                    y) {
   if (is.null(RSS)) {
-    RSS <- .RSS(
-      epsilonhat = epsilonhat,
+    RSS <- RSS(
       X = X,
-      y = y,
-      betahat = betahat
+      y = y
     )
+    n <- nrow(X)
   }
-  unbiased <- RSS / (n - k)
-  biased <- RSS / n
-  if (type == "unbiased") {
-    return(unbiased)
-  }
-  if (type == "biased") {
-    return(biased)
-  }
-  if (type == "both") {
-    return(
-      c(
-        unbiased = unbiased,
-        biased = biased
-      )
-    )
-  }
+  RSS / n
 }
 
 #' @author Ivan Jacob Agaloos Pesigan
@@ -96,12 +103,30 @@
 #' @inherit .sigma2epsilonhat return description references
 #' @export
 sigma2epsilonhat <- function(X,
-                             y,
-                             type = "unbiased") {
+                             y) {
   .sigma2epsilonhat(
     RSS = NULL,
-    type = type,
-    betahat = NULL,
+    n = NULL,
+    k = NULL,
+    X = X,
+    y = y
+  )
+}
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Residual Variance \eqn{\hat{\sigma}_{\varepsilon \ \textrm{biased}}^{2}}
+#'
+#' @family residual variance functions
+#' @keywords sigma2epsilonhat
+#' @inheritParams .sigma2epsilonhatbiased
+#' @inherit .sigma2epsilonhatbiased return description references
+#' @export
+sigma2epsilonhatbiased <- function(X,
+                                   y) {
+  .sigma2epsilonhatbiased(
+    RSS = NULL,
+    n = NULL,
     X = X,
     y = y
   )
